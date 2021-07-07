bc4a4519a471a42f306ed0a89ae1e698eeb207a5
Abdullah Alomari <85848980+assao11@users.noreply.github.com>
2021-07-07 18:16:41 +0300

    Add files via upload

---
+++ six_servo.ino
@@ -0,0 +1,63 @@
+#include <Servo.h>
+
+
+int s1 = 0;
+
+int s4 = 0;
+
+Servo servo_1;
+
+Servo servo_2;
+
+Servo servo_3;
+
+Servo servo_4;
+
+Servo servo_5;
+
+Servo servo_6;
+
+void setup()
+{
+  servo_1.attach(9, 500, 2500);
+
+  servo_2.attach(5, 500, 2500);
+
+  servo_3.attach(3, 500, 2500);
+
+  servo_4.attach(6, 500, 2500);
+
+  servo_5.attach(10, 500, 2500);
+
+  servo_6.attach(11, 500, 2500);
+
+}
+
+void loop()
+{
+  s1 = 0;
+  for (s1 = 1; s1 <= 89; s1 += 1) {
+    servo_1.write(s1);
+    servo_2.write(s1);
+    servo_3.write(s1);
+    delay(20); // Wait for 20 millisecond(s)
+  }
+  for (s1 = 1; s1 >= 89; s1 -= 1) {
+    servo_1.write(s1);
+    servo_2.write(s1);
+    servo_3.write(s1);
+  }
+
+  s4 = 0;
+  for (s4 = 1; s4 <= 89; s4 += 1) {
+    servo_4.write(s4);
+    servo_5.write(s4);
+    servo_6.write(s4);
+    delay(20); // Wait for 20 millisecond(s)
+  }
+  for (s4 = 1; s4 >= 89; s4 -= 1) {
+    servo_4.write(s4);
+    servo_5.write(s4);
+    servo_6.write(s4);
+  }
+}
